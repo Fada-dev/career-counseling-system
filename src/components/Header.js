bcad8c52e4f33fd69ebeae44912c43881807d3d0
Fada-dev <anthonyaffulkwabenabroni@gmail.com>
2025-01-19 07:47:38 +0000

    add Header component with styling and schedule session button

---
+++ src/components/Header.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { FaRegCalendarAlt, FaHome } from "react-icons/fa";
+
+
+const Header = () => {
+  return (
+    <div>
+      <div className="header">
+        <div className="header-links-container">
+          <div className="schedule-session-button">
+            <FaRegCalendarAlt
+              style={{ marginRight: "5px", fontSize: "18px" }}
+            />
+            <span>Schedule Session</span>
+          </div>
+          <FaHome style={{ color: "white", fontSize: "25px" }} />
+        </div>
+      </div>
+      <div className="logo-section">
+        <div className="logo">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/9/98/UoG_CoA_2017.svg"
+            alt="University Logo"
+          />
+        </div>
+        <div className="unit-name">
+          <span className="university-name">UNIVERSITY OF</span>
+          <br />
+          <span className="ghana">GHANA</span>
+          <br />
+          <span className="unit">CAREERS AND COUNSELING CENTER</span>
+        </div>
+      </div>
+
+    </div>
+  );
+};
+
+export default Header;
